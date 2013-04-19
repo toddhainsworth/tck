338f6355ad8ef46004094b3defa856cae634163e
GeissT <geisst-au@hotmail.com>
2013-04-19 22:11:08 +0930

    new ticket function

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,11 +2,32 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
-  
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(params[:ticket])
+
+    if @ticket.save
+      redirect_to ticket_path @ticket.id
+    else
+      redirect_to new_ticket_path
+    end
+  end
+
   def show
     @ticket = Ticket.find(params[:id])
 
     rescue ActiveRecord::RecordNotFound
       redirect_to root_path
+  end
+
+  def destroy
+    ticket = Ticket.find(params[:id])
+
+    ticket.destroy
+    redirect_to root_path
   end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,3 @@
 class Ticket < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :description, :author
 end
