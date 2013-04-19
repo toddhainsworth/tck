0e6f369ea08146e8db50cc918ad26e160f8cd242
GeissT <geisst-au@hotmail.com>
2013-04-19 19:49:01 +0930

    index and show tickets done

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -2,4 +2,11 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
+  
+  def show
+    @ticket = Ticket.find(params[:id])
+
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+  end
 end
